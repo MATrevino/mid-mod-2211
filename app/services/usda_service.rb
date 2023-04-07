9ed6eda74c6abc62db2fe2ae0585e68714c4f200
Meredith Trevino <meredithtrevino3@gmail.com>
2023-04-07 09:17:41 -0600

    adds service file and spec test for service

---
+++ app/services/usda_service.rb
@@ -0,0 +1,21 @@
+require 'faraday'
+
+class UsdaService
+
+  def self.search_foods(search_term)
+    get_url("/fdc/v1/foods/search?query=#{search_term}")
+  end
+  
+  private
+  
+  def self.get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+  
+  def self.conn 
+    Faraday.new(url: "https://api.nal.usda.gov") do |faraday|
+      faraday.params['api_key'] = ENV['USDA_API_KEY']
+    end
+  end
+end
---
+++ spec/services/usda_service_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe UsdaService do
+  describe 'class methods' do
+    it '.search_foods' do
+      search_results = UsdaService.search_foods('sweet potatoes')
+
+      expect(search_results).to be_a(Hash)
+      expect(search_results[:foods]).to be_an(Array)
+      expect(search_results[:foods].first).to have_key(:description)
+
+    end
+  end
+end
